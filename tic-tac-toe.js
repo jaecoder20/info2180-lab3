b31adf675f9142a40cc6f37c37ac642551615302
Rojae Wedderburn <rojaewedderburn@gmail.com>
2023-10-19 22:41:11 -0500

    Made squares clickable, colored X's and Os, did not implement winner logic

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,40 @@
 window.addEventListener('load', function() {
+    let gameState = [];
     let board = document.getElementById('board');
     let divs = board.getElementsByTagName('div');
-    console.log(divs);
+    setSqaureDivs(divs);
+    add_X_O(divs);
 
+    
+
+});
+
+
+let setSqaureDivs = function(divs){
     for(var i = 0; i < divs.length; i++) {
         divs[i].classList.add('square');
     }
-});
+}
+
+let add_X_O = function(divs){
+    x = true;
+    for(let i = 0; i < divs.length; i++) {
+        if (x==true){
+            divs[i].addEventListener('click', function(){
+                divs[i].classList.add("X");
+                divs[i].textContent = "X";
+            });
+            x=false;
+        }
+        else{
+            divs[i].addEventListener('click', function(){
+                divs[i].classList.add("O")
+                divs[i].textContent = "O";
+            });
+            x=true;
+        }
+    }
+}
+
+
+
